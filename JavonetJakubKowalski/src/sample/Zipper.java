f95e3f0dc7c3dd666624a099e3dbbce367a0b70a
JakubKowalski1997 <kubabartek1@op.pl>
2017-06-12 23:57:40 +0200

    Add files via upload
    Powinno działać

---
+++ JavonetJakubKowalski/src/sample/Zipper.java
@@ -0,0 +1,36 @@
+package sample;
+
+import com.javonet.JavonetException;
+import com.javonet.JavonetFramework;
+import com.javonet.api.NType;
+
+
+/**
+ * Created by Jakub Kowalski on 11.06.2017.
+ */
+public class Zipper {
+
+    //String extractPath = "C:\\Users\\Jakub Kowalski\\IdeaProjects\\JavonetApp\\extract1234";
+
+    public void zipIt(String a, String b) throws JavonetException {
+        try {
+            com.javonet.Javonet.activate("kubabartek1@op.pl", "o8AK-Rz4n-Me67-z3G9-Wn46", JavonetFramework.v40);
+            com.javonet.Javonet.addReference("System.Windows.Forms");
+            com.javonet.Javonet.addReference("System.IO.Compression");
+            com.javonet.Javonet.addReference("System.IO.Compression.FileSystem.dll");
+
+            //com.javonet.Javonet.getType("MessageBox").invoke("Show", "Hello from .NET!");
+
+            String start = a;
+            String zipPath = "C:\\Users\\Jakub Kowalski\\IdeaProjects\\JavonetApp\\" + b + ".zip";
+
+            NType zip = com.javonet.Javonet.getType("ZipFile");
+            zip.invoke("CreateFromDirectory", start, zipPath);
+            //zip.invoke("ExtractToDirectory", zipPath, extractPath);
+
+        }catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+}
